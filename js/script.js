48f637082abe668262456d833a5ff89388391a59
charliekaks <kaksunchained@gmail.com>
2017-06-17 15:47:54 +0300

    added a readme and licence

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
-var score =0;
-function playerDetails(playerName,score){
-  this.playerName = playerName;
+function gameScore(score){
   this.score = score;
 }
-playerDetails.prototype.score = function () {
-  return score += Math.floor((Math.random()*6)+1);
+score.prototype.addingScore = function () {
+  return this.score+=Math.floor((Math.random()*6)+1);
 };
-  var pName1= $("#playerOne").val();
-  var pName2= $("#playerTwo").val();
-  var total = 0;
-  var player1 = new playerDetails(pName1,0);
-  var player2 = new playerDetails(pName2,0);
 $(document).ready(function(){
-  $("button#role1").click(function(){
-      if (player1.score==1) {
-        alert("switch to next player`");
-      }else {
-        player1.score += total;
+  $(".player1").submit(function(event){
+    event.preventDefault();
+    var player1 = $("#playerOne").val();
+  });
 
-      }
-      console.log(total);
-      $(".resultPlayer1").text(total);
+
+  $(".player2").submit(function(event){
+    event.preventDefault();
+    var player2 = $("#playerTwo").val();
+  });
+
+
+  $("button#role1").click(function(){
+    var score1 = new gameScore();
+    if (true) {
+
+    }
+
   });
   $("button#role2").click(function(){
 
   });
-
 });
---
+++ readMe.md
@@ -0,0 +1,22 @@
+# PigDice game
+
+## project goals
+_use constructors, objects, prototypes and methods in javascript_
+## Game instructions
+_Pig Dice is a simple and fun game _
+1. there are two players, each taking turns to play by spinning a die
+2. On a turn, a player rolls the die repeatedly until either:
+
+
+A 1 is rolled
+The player chooses to hold (stop rolling)
+If a 1 is rolled, that player's turn ends and no points are earned.
+
+If the player chooses to hold, all of the points rolled during that turn are added to his or her score._
+### specifications
+1. create object to hold both the points and player name
+2. create a prototype to add the scores of the player
+3. create a user interface
+4. create a button that enable and disable buttons when players take turns
+5. create a hold button that enables the hold function
+ *author is [charliekaks]*
