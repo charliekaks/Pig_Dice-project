3853b018d15e2fa41db5dc0dadc8353f6622eeb2
charliekaks <kaksunchained@gmail.com>
2017-06-17 18:22:40 +0300

    got the random generator to work

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
-function gameScore(score){
+//business logic
+function gameScore(name,score){
+  this.name =name;
   this.score = score;
 }
-score.prototype.addingScore = function () {
+gameScore.prototype.addingScore = function (score) {
   return this.score+=Math.floor((Math.random()*6)+1);
 };
+// user interface logic
 $(document).ready(function(){
   $(".player1").submit(function(event){
     event.preventDefault();
     var player1 = $("#playerOne").val();
+    console.log(player1);
+
+    $("button#role1").click(function(){
+      var score1 = new gameScore( player1,0);
+      console.log("error");
+      console.log(score1.addingScore());
+
+    });
   });
 
 
   $(".player2").submit(function(event){
     event.preventDefault();
     var player2 = $("#playerTwo").val();
+    $("button#role2").click(function(){
+
+    });
   });
 
-
-  $("button#role1").click(function(){
-    var score1 = new gameScore();
-    if (true) {
-
-    }
-
-  });
-  $("button#role2").click(function(){
-
-  });
 });
